f6aaae391bb01e51ad88a2d10777407e02af4a45
iwilliams83 <indirajamurath@gmail.com>
2018-06-10 15:11:02 +0000

    automatically backed up by learn

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -2,5 +2,12 @@ class Teacher
 
   KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
 
+    attr_accessor :first_name, :last_name
+
+    def initialize
+      @first_name = first_name
+      @last_name = last_name
+    end
   
+
 end
